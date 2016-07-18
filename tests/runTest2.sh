50b2ff5336dc2a66115e2e5e000e490a3361b1fa
Felix Seiferling <felix.seiferling@gmail.com>
2016-07-18 11:10:42 +0200

    some test cases working. looking good. trying to at vnlin source terms now

---
+++ tests/runTest2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CODEDIR=~/NEOART_2.0/
+
+echo $CODEDIR
+
+cd $CODEDIR
+
+cp tests/input/input_test2.dat input.dat
+echo 'COLLISIONAL PS REGIME:'
+
+./neoart.x
+echo 'ION HEAT FLUX SHOULD BE SHOULD BE -0.80'
